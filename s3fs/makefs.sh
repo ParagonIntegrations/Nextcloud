9bdbaabed3b88c43dc02170b3e36a83b2a8c6df4
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-07-28 16:10:46 +0200

    added makefs.sh to create the filesystem

---
+++ s3fs/makefs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ..
+mkdir -p datadir/nextcloud/main
+mkdir -p datadir/nextcloud/bucket
+mkdir -p datadir/mariadb
+mkdir -p datadir/proxy/certs
+mkdir -p datadir/proxy/html
+mkdir -p datadir/proxy/vhost
