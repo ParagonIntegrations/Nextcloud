8c4c890ffce73e0c5c0b59df91ca58caf980dd2d
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-07-28 12:43:49 +0200

    added debug

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 docker-compose build --pull
+mkdir -p datadir/nextcloud/data
+mkdir -p datadir/nextcloud/main
+mkdir -p datadir/nextcloud/ssddata
 docker-compose up -d
 sleep 120
 docker exec --user www-data nextcloud php occ config:system:set overwriteprotocol --value="https"
