03031c019ab7a9671918d620b11963cc6a8a53ed
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-03 08:55:32 +0200

    debugging

--- s3fs/empty.sh
+++ s3fs/empty.sh
@@ -2,6 +2,23 @@
 
 echo "Empty.sh started"
 DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
-. trap.sh
 
+exit_script() {
+    SIGNAL=$1
+    echo "Caught $SIGNAL! Unmounting ${DEST}..."
+    fusermount -uz ${DEST}
+    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
+    if [ -n "$s3fs" ]; then
+        echo "Forwarding $SIGNAL to $s3fs"
+        kill -$SIGNAL $s3fs
+    fi
+    trap - $SIGNAL # clear the trap
+    exit $?
+}
+
+trap "exit_script INT" INT
+trap "exit_script TERM" TERM
+trap "exit_script QUIT" QUIT
+
+echo "about to start tail"
 tail -f /dev/null
