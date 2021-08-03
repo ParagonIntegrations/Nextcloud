d46482c0a90139991ee90af06d4bc11520fb0ed0
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-03 10:08:04 +0200

    doing test with traptester.sh

--- s3fs/traptester.sh
+++ s3fs/traptester.sh
@@ -1,10 +1,22 @@
 #! /usr/bin/env sh
-# main_script.sh
 
-trap "echo 'caught exit signal: SIGHUP'; exit 1" SIGHUP
-trap "echo 'caught exit signal: SIGINT'; exit 1" SIGINT
-trap "echo 'caught exit signal: SIGTERM'; exit 1" SIGTERM
-trap "echo 'caught exit signal: SIGQUIT'; exit 1" SIGQUIT
+exit_script() {
+    SIGNAL=$1
+    echo "Caught $SIGNAL! Unmounting ${DEST}..."
+#    fusermount -uz ${DEST}
+#    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
+#    if [ -n "$s3fs" ]; then
+#        echo "Forwarding $SIGNAL to $s3fs"
+#        kill -$SIGNAL $s3fs
+#    fi
+#    trap - $SIGNAL # clear the trap
+    exit 1
+}
+
+trap "exit_script SIGHUP" SIGHUP
+trap "exit_script SIGINT" SIGINT
+trap "exit_script SIGTERM" SIGTERM
+trap "exit_script SIGQUIT" SIGQUIT
 
 while true
   do sleep 1
