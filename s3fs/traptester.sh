d26463c4a9af2e3ec31af565ce9129e36e6d3165
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-03 10:02:30 +0200

    doing test with traptester.sh

--- s3fs/traptester.sh
+++ s3fs/traptester.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/env sh
 # main_script.sh
 
-trap "echo 'caught exit signal'; exit 1" SIGHUP SIGINT SIGTERM
+trap "echo 'caught exit signal: SIGHUP'; exit 1" SIGHUP
+trap "echo 'caught exit signal: SIGINT'; exit 1" SIGINT
+trap "echo 'caught exit signal: SIGTERM'; exit 1" SIGTERM
+trap "echo 'caught exit signal: SIGQUIT'; exit 1" SIGQUIT
 
 while true
   do sleep 1
