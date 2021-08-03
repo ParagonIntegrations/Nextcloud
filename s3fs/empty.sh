7d769b90428e7aa058b2137cb03ddababdb13626
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-03 09:17:44 +0200

    change supervisord.conf to debug lvl

--- s3fs/empty.sh
+++ s3fs/empty.sh
@@ -2,23 +2,6 @@
 
 echo "Empty.sh started"
 DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
+. trap.sh
 
-exit_script() {
-    SIGNAL=$1
-    echo "Caught $SIGNAL! Unmounting ${DEST}..."
-    fusermount -uz ${DEST}
-    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
-    if [ -n "$s3fs" ]; then
-        echo "Forwarding $SIGNAL to $s3fs"
-        kill -$SIGNAL $s3fs
-    fi
-    trap - $SIGNAL # clear the trap
-    exit SIGCHLD
-}
-
-trap "exit_script INT" SIGINT
-trap "exit_script TERM" SIGTERM
-trap "exit_script QUIT" SIGQUIT
-
-echo "about to start tail"
 tail -f /dev/null
