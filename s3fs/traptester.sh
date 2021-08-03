bc3c15c4e548023f863a480cb8d9f1b14d74e7e5
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-08-03 10:16:45 +0200

    doing test with traptester.sh

--- s3fs/traptester.sh
+++ s3fs/traptester.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env sh
 
+DEST=${AWS_S3_MOUNT:-/opt/s3fs/bucket}
+
 exit_script() {
     SIGNAL=$1
     echo "Caught $SIGNAL! Unmounting ${DEST}..."
-#    fusermount -uz ${DEST}
-#    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
-#    if [ -n "$s3fs" ]; then
-#        echo "Forwarding $SIGNAL to $s3fs"
-#        kill -$SIGNAL $s3fs
-#    fi
-#    trap - $SIGNAL # clear the trap
+    fusermount -uz ${DEST}
+    s3fs=$(ps -o pid= -o comm= | grep s3fs | sed -E 's/\s*(\d+)\s+.*/\1/g')
+    if [ -n "$s3fs" ]; then
+        echo "Forwarding $SIGNAL to $s3fs"
+        kill -$SIGNAL $s3fs
+    fi
+    trap - $SIGNAL # clear the trap
     exit 1
 }
 
